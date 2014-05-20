6dfe9574b0e124460beaff7ca21de38886d71678
Alex <Alex@Alex-PC>
2014-05-20 18:39:46 +0200

    structural done

---
+++ src/main/java/com/tp/alex/designpatterns/structural/bridge/BigBus.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tp.alex.designpatterns.structural.bridge;
+
+/**
+ *
+ * @author Alex
+ */
+public class BigBus extends Vehicle {
+
+	public BigBus(Engine engine) {
+		this.weightInKilos = 3000;
+		this.engine = engine;
+	}
+
+	@Override
+	public void drive() {
+		System.out.println("\nThe big bus is driving");
+		int horsepower = engine.go();
+		reportOnSpeed(horsepower);
+	}
+    
+}
---
+++ src/main/java/com/tp/alex/designpatterns/structural/decorator/LegDecorator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tp.alex.designpatterns.structural.decorator;
+
+/**
+ *
+ * @author Alex
+ */
+public class LegDecorator extends AnimalDecorator {
+
+	public LegDecorator(Animal animal) {
+		super(animal);
+	}
+
+	@Override
+	public void describe() {
+		animal.describe();
+		System.out.println("I have legs.");
+		dance();
+	}
+
+	public void dance() {
+		System.out.println("I can dance.");
+	}
+
+    
+}
